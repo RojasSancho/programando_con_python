baa4272c4a30b29b2723ea588b7070168743fba9
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2025-07-17 17:27:55 -0600

    Commit version final Listas

--- primeras_listas.py
+++ primeras_listas.py
@@ -1,7 +1,7 @@
 #Primera lista
 mi_lista=["Flori", "Josue", "Fabi", "Josue"]
 
-#Devolver elementos de la listade
+#1 - Devolver elementos de la listade
 print(mi_lista[:]) #Sintaxis para imprimir la lista completa
 
 print(mi_lista[2]) #Sintaxis para imprimir el elemento de un indice especifico
@@ -14,31 +14,31 @@ print(mi_lista[:2]) #Devuelve desde el inicio hasta uno antes del indicado
 
 print(mi_lista[1:]) #Devuelve desde el indice indicado hasta el final de la lista
 
-#Agregar elementos a una lista
+#2 - Agregar elementos a una lista
 mi_lista.append("Maria") #Agrega elemento al final de la lista
 mi_lista.insert(0, "Brayan") #Parametros (indice, elemento) / Inserta elemento en indice concreto
 mi_lista.extend(["Emilce", "Dilan", "Mauricio"]) #Anade varios elementos al final de la lista
 
-#Devuelve el indice donde se encuentra el elemento dado
+#3 - Devuelve el indice donde se encuentra el elemento dado
 print(mi_lista.index("Dilan")) 
 
-#Verificar si un elemento pertenece o esta en una lista (Boolean)
+#4 - Verificar si un elemento pertenece o esta en una lista (Boolean)
 print("Brayan" in mi_lista)
 
-#Eliminar elementos de la lista
+#5 - Eliminar elementos de la lista
 mi_lista.remove("Josue") #Elimina un elemento especifico de la lista, el primero que encuentre
 mi_lista.pop() #Elimina el ultimo elemento de la lista
 
 print(mi_lista[:])
 
 
-#Unir dos lista diferentes
+#6 - Unir dos lista diferentes
 mi_lista2 = [2, 3, 4, 5]
 listas_unidas = mi_lista + mi_lista2 #Operador de suma
 
 print(listas_unidas[:]) 
 
-#Repetidor (hacer una lista repitiendo una serie de elementos)  
+#7 - Repetidor (hacer una lista repitiendo una serie de elementos)  
 
 lista_repeticion = [1, 2, 3, 4, 5] * 4 #Operador de resta
 print(lista_repeticion[:]) 
