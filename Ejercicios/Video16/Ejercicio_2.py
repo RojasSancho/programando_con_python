bf096e00a7bfbdaa1d1514a22d05e2d0d25d0bb9
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2025-07-19 23:09:40 -0600

    ejercicios video 16 listos

--- Ejercicios/Video16/Ejercicio_2.py
+++ Ejercicios/Video16/Ejercicio_2.py
@@ -3,3 +3,19 @@
 # podrá tener menos de 8 caracteres ni espacios en blanco. Si la contraseña es correcta, 
 # el programa imprime “Contraseña OK”. En caso contrario imprime “Contraseña 
 # errónea” 
+
+contrasena_usuario = input("Ingrese su nueva contrasena: ")
+validez=True
+
+if len(contrasena_usuario) < 8:
+    validez=False
+
+for caracter in contrasena_usuario:
+    if caracter==" ":
+        validez=False
+
+if validez==True:
+    print("Contrasena OK")
+else:
+    print("Contrasena erronea")
+
--- Ejercicios/Video16/Ejercicio_3.py
+++ Ejercicios/Video16/Ejercicio_3.py
@@ -2,3 +2,19 @@
 # • Crea un programa que evalúe si una dirección de correo electrónico es válida o no en 
 # función de si tiene “@” o “.” Hay que tener en cuenta que la dirección se considera 
 # correcta si solo tiene una “@” y si tiene uno o más “.” 
+
+correo_electronico = input("Ingrese el nuevo correo: ")
+contador_arroba=0
+contador_puntos=0
+
+for caracter in correo_electronico:
+    if caracter=="@":
+        contador_arroba += 1
+
+    if caracter==".":
+        contador_puntos +=1
+
+if contador_arroba==1 and contador_puntos>0:
+    print("Correo valido")
+else:
+    print("Correo invalido")
